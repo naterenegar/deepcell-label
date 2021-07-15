f004a74338bb90d3d955f1b26213aeca35560467
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-07-15 09:33:20 -0700

    Mock up fetching channel names with example data

--- visualizer/src/service/raw/rawMachine.js
+++ visualizer/src/service/raw/rawMachine.js
@@ -14,6 +14,56 @@ import createGrayscaleMachine from './grayscaleMachine';
 
 const { pure, respond } = actions;
 
+function fetchChannelNames(context) {
+  return Promise.resolve({
+    0: 'Au',
+    1: 'Background',
+    2: 'C',
+    3: 'CC3',
+    4: 'CD11c',
+    5: 'CD138',
+    6: 'CD14',
+    7: 'CD16',
+    8: 'CD163',
+    9: 'CD20',
+    10: 'CD206',
+    11: 'CD209',
+    12: 'CD21',
+    13: 'CD3',
+    14: 'CD31',
+    15: 'CD4',
+    16: 'CD45',
+    17: 'CD45RO',
+    18: 'CD57',
+    19: 'CD68',
+    20: 'CD8',
+    21: 'Ca',
+    22: 'Fe',
+    23: 'Foxp3',
+    24: 'GLUT1',
+    25: 'GranzymeB',
+    26: 'H3K27me3',
+    27: 'H3K9Ac',
+    28: 'HH3',
+    29: 'HLA-DR-DQ-DP',
+    30: 'IDO',
+    31: 'Ki67',
+    32: 'Lag3',
+    33: 'MPO',
+    34: 'Na',
+    35: 'PD-L1',
+    36: 'PD1',
+    37: 'SMA',
+    38: 'Si',
+    39: 'TIM3',
+    40: 'Ta',
+    41: 'Tbet',
+    42: 'Tryptase',
+    43: 'Vimentin',
+    44: 'pS6',
+  });
+}
+
 const preloadState = {
   entry: 'startPreload',
   on: {
@@ -128,6 +178,10 @@ const createRawMachine = (projectId, numChannels, numFrames) =>
         grayscale: null,
       },
       entry: ['spawnChannels', 'spawnColorModes'],
+      invoke: {
+        src: fetchChannelNames,
+        onDone: { actions: 'setChannelNames' },
+      },
       type: 'parallel',
       states: {
         preload: preloadState,
@@ -191,6 +245,9 @@ const createRawMachine = (projectId, numChannels, numFrames) =>
           },
           channelNames: ({ numChannels }) =>
             [...Array(numChannels).keys()].map(i => `channel ${i}`),
+        }),
+        setChannelNames: assign({
+          channelNames: (_, event) => Object.values(event.data),
         }),
         spawnColorModes: assign({
           grayscale: context =>
