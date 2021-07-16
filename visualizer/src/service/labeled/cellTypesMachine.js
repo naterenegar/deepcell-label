e54ee69c31fcc6e307922d38b925ac99b1140e26
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-07-15 23:42:42 -0700

    Create separate exampleData file Provide both instance wise & cell type wise labels

--- visualizer/src/service/labeled/cellTypesMachine.js
+++ visualizer/src/service/labeled/cellTypesMachine.js
@@ -1,94 +1,5 @@
 import { assign, Machine } from 'xstate';
-
-const exampleData = {
-  cellTypes: {
-    0: {
-      name: 'unassigned',
-      channelNames: null,
-      channels: null,
-    },
-    1: {
-      name: 'CD4T',
-      channelNames: ['CD45', 'CD3', 'CD4'],
-      channels: [16, 13, 15],
-    },
-    2: {
-      name: 'CD8T',
-      channelNames: ['CD45', 'CD3', 'CD8'],
-      channels: [16, 13, 20],
-    },
-    3: {
-      name: 'B cell',
-      channelNames: ['CD45', 'CD20', 'CD21'],
-      channels: [16, 9, 12],
-    },
-    4: {
-      name: 'FDC',
-      channelNames: ['CD45', 'CD21'],
-      channels: [16, 12],
-    },
-    5: {
-      name: 'Endothelial',
-      channelNames: ['CD31'],
-      channels: [14],
-    },
-    6: {
-      name: 'Fibroblast',
-      channelNames: ['SMA'],
-      channels: [37],
-    },
-    7: {
-      name: 'Other immune',
-      channelNames: ['CD45'],
-      channels: [16],
-    },
-    8: {
-      name: 'CD11c+ DC',
-      channelNames: ['CD45', 'HLADR', 'CD11c'],
-      channels: [16, 4],
-    },
-    9: {
-      name: 'CD209+ DC',
-      channelNames: ['CD45', 'HLADR', 'CD11c', 'CD209'],
-      channels: [16, 4, 11],
-    },
-    10: {
-      name: 'CD14+ mono/mac',
-      channelNames: ['CD45', 'CD14'],
-      channels: [16, 6],
-    },
-    11: {
-      name: 'CD68+ mac',
-      channelNames: ['CD45', 'CD68'],
-      channels: [16, 19],
-    },
-    12: {
-      name: 'Neutrophil',
-      channelNames: ['MPO'],
-      channels: [33],
-    },
-    13: {
-      name: 'T_reg',
-      channelNames: ['CD45', 'CD3', 'Foxp3'],
-      channels: [16, 13, 23],
-    },
-    14: {
-      name: 'Tfh',
-      channelNames: ['CD45', 'CD3', 'CD4', 'PD1'],
-      channels: [16, 13, 15, 36],
-    },
-    15: {
-      name: 'Plasma',
-      channelNames: ['CD138'],
-      channels: [5],
-    },
-    16: {
-      name: 'CD209+ CD206+ mac',
-      channelNames: ['CD45', 'CD68', 'CD163', 'CD209', 'CD206'],
-      channels: [16, 19, 8, 11, 10],
-    },
-  },
-};
+import exampleData from './exampleData';
 
 function fetchCellTypes(context) {
   return Promise.resolve(exampleData);
@@ -102,6 +13,9 @@ const createCellTypesMachine = (projectId, feature) =>
         projectId,
         feature,
         cellTypes: {},
+        cellTypeLabels: {},
+        instanceLabels: {},
+        cellType: null,
       },
       initial: 'loading',
       states: {
@@ -110,17 +24,35 @@ const createCellTypesMachine = (projectId, feature) =>
             src: fetchCellTypes,
             onDone: {
               target: 'idle',
-              actions: 'saveCellTypes',
+              actions: [
+                'saveCellTypes',
+                'saveCellTypeLabels',
+                'saveInstanceLabels',
+              ],
             },
           },
         },
-        idle: {},
+        idle: {
+          on: {
+            SET_CELL_TYPE: { actions: 'setCellType' },
+            ASSIGN_CELL_TYPE: { actions: 'assignCellType' },
+          },
+        },
       },
     },
     {
       guards: {},
       actions: {
+        setCellType: assign({
+          cellType: (_, { cellType }) => cellType,
+        }),
         saveCellTypes: assign({ cellTypes: (_, { data }) => data.cellTypes }),
+        saveCellTypeLabels: assign({
+          cellTypeLabels: (_, { data }) => data.cellTypeLabels,
+        }),
+        saveInstanceLabels: assign({
+          instanceLabels: (_, { data }) => data.instanceLabels,
+        }),
       },
     }
   );
